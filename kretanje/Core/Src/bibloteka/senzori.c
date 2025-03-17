651d375789be4ecc77212cefe28fd9abab59c3e9
Nikola Stojic <94862513+stojicnnnn@users.noreply.github.com>
2025-03-17 11:27:34 +0100

    Sensors file cleared up a bit

--- kretanje/Core/Src/bibloteka/senzori.c
+++ kretanje/Core/Src/bibloteka/senzori.c
@@ -14,38 +14,38 @@ void init_senzor(){
 
 	RCC->AHB1ENR |= (1 << 2); // c port
 	RCC->AHB1ENR |= (1 << 0);  // a port
-	//1.  C12
-			GPIOC->MODER &= ~(0b11 << 12*2);  // kliruje
-			GPIOC->MODER |= (0b00 << 12*2);  // stavlja ga na input
-			GPIOC->PUPDR &= ~(0b11 << 12*2);  // kliruje pin za input bitova
-			GPIOC->PUPDR |= (0b10 << 12*2);  // Setujem pull-down
+	//1.  C12 front sensors
+			GPIOC->MODER &= ~(0b11 << 12*2); //Clears the mode bits for pin 12 of GPIO port C.
+			GPIOC->MODER |= (0b00 << 12*2);  //Sets the mode bits for pin 12 to 00, which configures it as an input.
+			GPIOC->PUPDR &= ~(0b11 << 12*2);  //Clears the pull-up/pull-down bits for pin 12 of GPIO port C.
+			GPIOC->PUPDR |= (0b10 << 12*2);  //Sets the pull-up/pull-down bits for pin 12 to 10, which configures it with a pull-down resistor.
 		//2.   C3
-			GPIOC->MODER &= ~(0b11 << 3*2);  // kliruje
-			GPIOC->MODER |= (0b00 << 3*2);  // stavlja ga na input
-			GPIOC->PUPDR &= ~(0b11 << 3*2);  // kliruje pin za input bitova
-			GPIOC->PUPDR |= (0b10 << 3*2);  // Setujem pull-down
+			GPIOC->MODER &= ~(0b11 << 3*2); 
+			GPIOC->MODER |= (0b00 << 3*2);  
+			GPIOC->PUPDR &= ~(0b11 << 3*2);  
+			GPIOC->PUPDR |= (0b10 << 3*2);  
 		//3.  A15
-			GPIOA->MODER &= ~(0b11 << 15*2);  // kliruje
-			GPIOA->MODER |= (0b00 << 15*2);  // stavlja ga na input
-			GPIOA->PUPDR &= ~(0b11 << 15*2);  // kliruje pin za input bitova
-			GPIOA->PUPDR |= (0b10 << 15*2);  // Setujem pull-down
+			GPIOA->MODER &= ~(0b11 << 15*2); 
+			GPIOA->MODER |= (0b00 << 15*2);  
+			GPIOA->PUPDR &= ~(0b11 << 15*2);  
+			GPIOA->PUPDR |= (0b10 << 15*2);  
 
-			//ZADNJA STRANA
+			//rear sensors
 			//1.  c13
-			GPIOC->MODER &= ~(0b11 << 13*2);  // kliruje
-			GPIOC->MODER |= (0b00 << 13*2);  // stavlja ga na input
-			GPIOC->PUPDR &= ~(0b11 << 13*2);  // kliruje pin za input bitova
-			GPIOC->PUPDR |= (0b10 << 13*2);  // Setujem pull-down
+			GPIOC->MODER &= ~(0b11 << 13*2); 
+			GPIOC->MODER |= (0b00 << 13*2);  
+			GPIOC->PUPDR &= ~(0b11 << 13*2);  
+			GPIOC->PUPDR |= (0b10 << 13*2);  
 			//2.   C0
-			GPIOC->MODER &= ~(0b11 << 0*2);  // kliruje
-			GPIOC->MODER |= (0b00 << 0*2);  // stavlja ga na input
-			GPIOC->PUPDR &= ~(0b11 << 0*2);  // kliruje pin za input bitova
-			GPIOC->PUPDR |= (0b10 << 0*2);  // Setujem pull-down
+			GPIOC->MODER &= ~(0b11 << 0*2); 
+			GPIOC->MODER |= (0b00 << 0*2);  
+			GPIOC->PUPDR &= ~(0b11 << 0*2);  
+			GPIOC->PUPDR |= (0b10 << 0*2);  
 			//3.  C2
-			GPIOC->MODER &= ~(0b11 << 2*2);  // kliruje
-			GPIOC->MODER |= (0b00 << 2*2);  // stavlja ga na input
-			GPIOC->PUPDR &= ~(0b11 << 2*2);  // kliruje pin za input bitova
-			GPIOC->PUPDR |= (0b10 << 2*2);  // Setujem pull-down*/
+			GPIOC->MODER &= ~(0b11 << 2*2); 
+			GPIOC->MODER |= (0b00 << 2*2);  
+			GPIOC->PUPDR &= ~(0b11 << 2*2);  
+			GPIOC->PUPDR |= (0b10 << 2*2);  */
 }
 
 
